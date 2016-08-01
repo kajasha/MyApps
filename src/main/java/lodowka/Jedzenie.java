569e75b42b2a74a32c42766f281a3596356d2fc4
Karola <yumi.chan@interia.pl>
2016-08-01 12:03:06 +0200

    Jedzenie

---
+++ src/main/java/lodowka/Jedzenie.java
@@ -0,0 +1,17 @@
+package lodowka;
+
+public class Jedzenie {
+
+    String nazwa;
+    String skladniki;
+
+    public Jedzenie(String nazwa, String skladniki) {
+
+        this.nazwa = nazwa;
+        this.skladniki = skladniki;
+    }
+
+    public String toString() {
+        return "Jedzenie: [" + nazwa + " - " + " " + skladniki + "]";
+    }
+}
---
+++ src/main/java/lodowka/JedzenieWeb.java
@@ -0,0 +1,7 @@
+package lodowka;
+
+public class JedzenieWeb {
+    public static void main(String[] args) {
+
+    }
+}
